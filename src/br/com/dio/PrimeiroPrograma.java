ecfbe1fa7ce6ef4d6c230b4c86c04c83748a27d9
Jopa <joaopaulocosta.r@hotmail.com>
2023-02-28 17:20:57 -0300

    create livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -2,14 +2,55 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
+import java.util.Objects;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro Livro = new Livro("O conde de Monte Cristo", 500);
+        System.out.println("Livro");
+
         /*  int a = 3;
         int b = 5;
 
         System.out.println("Hello World!" + (a+b));*/
+    }
+
+}
+
+class Livro {
+    private String nome;
+    private Integer numpag;
+
+
+    public Livro(String nome, Integer numpag) {
+        this.nome = nome;
+        this.numpag = numpag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumpag() {
+        return numpag;
+    }
+
+    public void setNumpag(Integer numpag) {
+        this.numpag = numpag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numpag=" + numpag +
+                '}';
     }
 }
